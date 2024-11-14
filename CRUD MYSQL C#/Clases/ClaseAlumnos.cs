ddd5f26375c0b14c4c462e1ac038ad2de4d76179
ana galvez <130255411+Ana-Galvez@users.noreply.github.com>
2024-11-14 00:27:34 -0300

    feat: clase para mostrar tabla

---
+++ CRUD MYSQL C#/Clases/ClaseAlumnos.cs
@@ -0,0 +1,31 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CRUD_MYSQL_C_.Clases
+{
+    internal class ClaseAlumnos
+    {
+        public void MostrarAlumnos(DataGridView tablaAlumnos)
+        {
+            try{
+                ClaseConexion objetoConexion = new ClaseConexion();
+                string query = "select * from alumnos";
+                tablaAlumnos.DataSource = null;
+                MySqlDataAdapter adapter = new MySqlDataAdapter(query,objetoConexion.EstablecerConexion());
+                DataTable dt = new DataTable();
+                adapter.Fill(dt);
+                tablaAlumnos.DataSource = dt;
+                objetoConexion.CerrarConexion();
+            }
+            catch(Exception ex) {
+                MessageBox.Show("No se pudo mostrar la información de la tabla. Error: " + ex.ToString());
+            }
+        }
+    }
+}
